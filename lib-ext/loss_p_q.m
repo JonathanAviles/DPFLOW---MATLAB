a27215fbc46a8afda412986bcd90e08e756f7941
Ahmed Elkholy <ahmed_elkholy@f-eng.tanta.edu.eg>
2022-12-22 17:55:50 +0300

    regup

--- lib-ext/loss_p_q.m
+++ lib-ext/loss_p_q.m
@@ -1,6 +1,7 @@
 function [p_loss,q_loss]=loss_p_q(branchInfo)
 % this identify the losses in ever phase per line 
 % the input is the result function the output of the dpflow toolbox
+%[p_loss,q_loss]=loss_p_q(branchInfo)
 
 % Author(s): <Ahmed M. Elkholy>
 % Copyright <2023> <A.M.Elkholy>
--- lib-ext/phas2seq.m
+++ lib-ext/phas2seq.m
@@ -1,4 +1,8 @@
 function [Vp,Vn,V0]=phas2seq(Va,Vb,Vc)
+%% this function introduces the way to calculate the negative and positive and zero sequences
+%% [Vp,Vn,V0]=phas2seq(Va,Vb,Vc)
+%% the value of the a=1<120 deg and this for the (abc) system that means the phase b have the angle -120 and phase c have angle 120
+
     a=exp(1j*((2*pi)/3));
     Vpn0=(1/3).*[1 a a^2;1 a^2 a;1 1 1]*[Va;Vb;Vc];
     Vp=Vpn0(1);
