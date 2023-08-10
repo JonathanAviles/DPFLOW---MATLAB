a3ab0ef981efd7a61a422bfcd9af9032fcac0f55
Ahmed Elkholy <ahmed_elkholy@f-eng.tanta.edu.eg>
2023-08-11 02:31:04 +0300

    update

--- lib-ext/phase1sym.m
+++ lib-ext/phase1sym.m
@@ -2,14 +2,13 @@ function Vxxx=phase1sym(Vyyy,flg)
 %% this function introduces the way to calculate the negative and positive and zero sequences
 %% Vabc=phase1sym(Vpno,'phase') Vpno=phase1sym(Vabc,'sym')
 %% the value of the a=1<120 deg and this for the (abc) system that means the phase b have the angle -120 and phase c have angle 120
-%% if the flg=phas >> convert from abc to pn0
-%% if the flg=2 >> convert from pn0 to abc
+%% if the flg=sym >> convert from abc to pn0
+%% if the flg=phase >> convert from pn0 to abc
+a=exp(1j*((2*pi)/3));
 switch flg
     case 'sym' 
-    a=exp(1j*((2*pi)/3));
     Vxxx=(1/3).*[1 a a^2;1 a^2 a;1 1 1]*Vyyy;
     case 'phase' 
-    a=exp(1j*((2*pi)/3));
     Vxxx=(3).*[1 a a^2;1 a^2 a;1 1 1]^-1*Vyyy;
     end
 end
--- lib-ext/unbalancing_R.m
+++ lib-ext/unbalancing_R.m
@@ -1,20 +1,15 @@
-function K_uN=unbalancing_Ratio(Result,id_bus,ver)
+function K_uN=unbalancing_R(Result,id_bus)
 %unbalancing_Ratio <one line description>
-%
-% unbalancing_Ratio() <explain usage>
-% 
 % the input is the result function the output of the dpflow toolbox
 % id_bus that we need to calculate the bus unbalancing ratio 
+% the output of this function is the unbalancing coefficient for zero and negative as vector K_uN=[k_0;k_1]
 
 % Author(s): <Ahmed M. Elkholy>
 % Copyright <2021> <A.M.Elkholy>
-
 v_a_u=Result.busInfo(id_bus,2)*exp(i*deg2rad(Result.busInfo(id_bus,2+3)));
 v_b_u=Result.busInfo(id_bus,3)*exp(i*deg2rad(Result.busInfo(id_bus,3+3)));
 v_c_u=Result.busInfo(id_bus,4)*exp(i*deg2rad(Result.busInfo(id_bus,4+3)));
-V_a_b_u=abs(v_a_u-v_b_u);
-V_b_c_u=abs(v_b_u-v_c_u);
-V_c_a_u=abs(v_c_u-v_a_u);
-U_r=(V_a_b_u^4+V_b_c_u^4+V_c_a_u^4)/(V_a_b_u^2+V_b_c_u^2+V_c_a_u^2)^2;
-K_uN=abs(sqrt((1-sqrt(3-6*U_r))/(sqrt(6*U_r+3)+1)));
+Vabc=[v_a_u;v_b_u;v_c_u];
+Vpno=phase1sym(Vabc,'sym');
+K_uN=abs([Vpno(3);Vpno(2)]./Vpno(1));
 end
